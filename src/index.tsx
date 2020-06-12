18454d53eb1dff682f0b9150915d5b095cd0bb58
Eduardo Vencovsky <eduvencovsky@gmail.com>
2020-06-12 12:25:33 -0300

    Firebase Config Added

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import firebase from 'firebase'
 import App from './App'
+
+const firebaseConfig = {
+  // put firebase config from the firebase console here
+}
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
